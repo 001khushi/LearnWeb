ff9e5c089268060d96ab3ac47c4905d9eac7043d
Khushi Saxena <aachalsaxena001@.gmail.com>
2022-02-12 16:03:02 +0530

    Practice

---
+++ javascript/javascript.js
@@ -0,0 +1,12 @@
+// console.log("Khushi");
+// var n=4;
+// var m=5;
+// console.log(n+m,4+5);
+// var str1="Khushi Saxena";
+// console.log(str1);
+function avg(a,b){
+    return (a+b)/2;
+}
+c=avg(4,6);
+console.log(c);
+// c=avg(14,6);
---
+++ javascript/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+console.log("Khushi");
+// var n=4;
+// var m=5;
+// console.log(n+m,4+5);
+// var str1="Khushi Saxena";
+// console.log(str1);
